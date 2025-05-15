c5aef12888f7206893c40d71eee5946a218e8189
Sparsh-I <sparsh.poonacha@gmail.com>
2025-05-15 16:04:56 +0530

    Started Red King

---
+++ Red King/Assets/Scripts/CardController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    [Header("Card Value")]
+    [SerializeField] private int value;
+    [SerializeField] private CardSuit suit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Red King/Assets/Scripts/CardSuit.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace
+{
+    public enum CardSuit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs,
+    }
+}
